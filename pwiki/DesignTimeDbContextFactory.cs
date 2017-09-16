0a36b5d1226e5cde3876d7bf8584805bc18e0719
mkhabibullin <safe.kzn@gmail.com>
2017-09-16 21:24:06 +0300

    DesignTimeDbContextFactory

---
+++ pwiki/DesignTimeDbContextFactory.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using pwiki.domain;
+using System.IO;
+
+namespace pwiki
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<PwikiDbContext>
+    {
+        public PwikiDbContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var builder = new DbContextOptionsBuilder<PwikiDbContext>();
+
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            builder.UseSqlServer(connectionString);
+
+            return new PwikiDbContext(builder.Options);
+        }
+    }
+}
