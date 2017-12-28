62421e17ed49cd8078b58aa67f5ef7cb43d05a4d
KhabibullinM <marat-011@yandex.ru>
2017-12-28 22:15:35 +0300

    +

--- pwiki.web/ClientApp/app/material.module.ts
+++ pwiki.web/ClientApp/app/material.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatButtonModule } from '@angular/material';
+import { MatButtonModule, MatGridListModule, MatCardModule } from '@angular/material';
 
 @NgModule({
-    imports: [MatButtonModule],
-    exports: [MatButtonModule],
+    imports: [MatButtonModule, MatGridListModule, MatCardModule],
+    exports: [MatButtonModule, MatGridListModule, MatCardModule],
 })
 export class MaterialModule { }
