51ce0a2f77d9bf0d8eb44958833721064bcc6a3d
KhabibullinM <marat-011@yandex.ru>
2017-12-30 18:34:02 +0300

    test

--- pwiki.web/webpack.config.js
+++ pwiki.web/webpack.config.js
@@ -6,7 +6,7 @@ const CheckerPlugin = require('awesome-typescript-loader').CheckerPlugin;
 
 module.exports = (env) => {
     // Configuration in common to both client-side and server-side bundles
-    const isDevBuild = !(env && env.prod);
+    const isDevBuild = true;// !(env && env.prod);
     const sharedConfig = {
         stats: { modules: false },
         context: __dirname,
@@ -17,7 +17,7 @@ module.exports = (env) => {
         },
         module: {
             rules: [
-                { test: /\.ts$/, include: /ClientApp/, use: ['awesome-typescript-loader?silent=true', 'angular2-template-loader'] },
+                { test: /\.ts$/, include: /ClientApp/, use: isDevBuild ? ['awesome-typescript-loader?silent=true', 'angular2-template-loader'] : '@ngtools/webpack' },
                 { test: /\.html$/, use: 'html-loader?minimize=false' },
                 { test: /\.css$/, use: [ 'to-string-loader', isDevBuild ? 'css-loader' : 'css-loader?minimize' ] },
                 { test: /\.(png|jpg|jpeg|gif|svg)$/, use: 'url-loader?limit=25000' }
