6c2ca049ec34832d00b56cc28caf4457188a7abd
KhabibullinM <marat-011@yandex.ru>
2018-01-19 20:04:25 +0300

    tarantool

---
+++ TarantoolTests/Program.cs
@@ -0,0 +1,39 @@
+﻿using ProGaudi.Tarantool.Client;
+using ProGaudi.Tarantool.Client.Model;
+using ProGaudi.Tarantool.Client.Model.Enums;
+using System;
+using System.Threading.Tasks;
+
+namespace TarantoolTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DoWork().Wait();
+
+            Console.WriteLine("Done");
+            Console.Read();
+        }
+
+        static async Task DoWork()
+        {
+            using (var box = await Box.Connect("operator:123123@localhost:3301"))
+            {
+                var schema = box.GetSchema();
+                var space = await schema.GetSpace("users");
+                var primaryIndex = await space.GetIndex("primary_id");
+                var data = await primaryIndex.Select<TarantoolTuple<string>,
+                            TarantoolTuple<string, string, string, string, long>>(
+                            TarantoolTuple.Create(String.Empty), new SelectOptions
+                            {
+                                Iterator = Iterator.All
+                            });
+                foreach (var item in data.Data)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+    }
+}
